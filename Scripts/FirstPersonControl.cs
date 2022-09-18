556ba14719cd9a0c5d9616346dd15825a95e4f51
Jared Blackburn <BlackJar72@yahoo.com>
2022-09-18 16:56:49 -0400

    Completed old input fly cam prototype

--- Scripts/FirstPersonControl.cs
+++ Scripts/FirstPersonControl.cs
@@ -8,17 +8,34 @@ namespace simcam
     /**
     This is for moving in a way typical of flying in a first person game 
     (i.e., a "fly-cam"), similar to creative and spectator modes in one 
-    well know game.
+    well know game.  This version uses the old input system; a version
+    using the new input system will come later.
     */
     public class FirstPersonControl : ICameraControl
     {
-        /*// Start is called before the first frame update
+        [SerializeField]
+        private float rotationSpeed = 1;
+        [SerializeField]
+        private float moveSpeed = 1;
+
+        public float headingAngle = 0;
+        public float tiltAngle = 0;
+
+        public Vector3 movement;
+
+        public Quaternion heading = Quaternion.identity;
+        public Quaternion tilt    = Quaternion.identity;
+
+
+        //*
+        // Start is called before the first frame update
         void Start()
         {
-            
-        }*/
+            Cursor.lockState = CursorLockMode.Locked;
+        }//*/
 
-        protected override void OnEnable() 
+       /*
+       protected override void OnEnable()
         {
             // Will this be needed here?  IDK.
         }
@@ -28,12 +45,53 @@ namespace simcam
         {
             // Will this be needed here?  IDK.            
         }
+        */
 
         // Update is called once per frame
         void Update()
         {
-            
+            AdjustHeading();
+            AdjustPitch();
+            SetRotation();
+            Move();
         }
+
+
+        void AdjustHeading() {
+            float rotation = Input.GetAxis("Mouse X") * rotationSpeed / Time.deltaTime;
+            headingAngle += rotation;
+            if(headingAngle> 360) headingAngle-= 360;
+            else if(headingAngle < 0) headingAngle += 360;
+            heading = Quaternion.Euler(0, headingAngle, 0);
+        }
+
+
+        void AdjustPitch() {
+            float rotation = Input.GetAxis("Mouse Y") * rotationSpeed / Time.deltaTime;
+            tiltAngle += rotation;
+            tiltAngle = Mathf.Clamp(tiltAngle, -80, 80);
+            tilt = Quaternion.Euler(tiltAngle, 0, 0);
+        }
+
+
+        void SetRotation() {
+            transform.rotation = heading * tilt;
+        }
+
+
+        void Move() {
+            /*Vector3*/ movement = Vector3.zero;
+            movement.z = Input.GetAxis("Vertical");
+            movement.x = Input.GetAxis("Horizontal");
+            movement = heading * movement;
+            if(Input.GetKey(KeyCode.LeftShift)) movement.y -= 1;
+            if(Input.GetKey(KeyCode.Space)) movement.y += 1;
+            movement.Normalize();
+            movement *= moveSpeed ;
+            movement *= Time.deltaTime;
+            transform.Translate(movement, Space.World);
+        }
+
     }
 
 }
